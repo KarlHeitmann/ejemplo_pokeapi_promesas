71e5e81408d3cf5a142f507d861356cd235c3f3f
Karl Heitmann <khf1988@gmail.com>
2022-12-28 20:57:15 -0300

    funciona axios :D

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const axios = require('axios');
+// import axios, {isCancel, AxiosError} from 'axios';
+// import axios from 'axios';
+
+// var axios = require('axios');
+// import axios from "axios"
 console.log("Hola mundo")
+
+
+
+var config = {
+  method: 'get',
+  url: 'https://pokeapi.co/api/v2/pokemon/ditto',
+  headers: { }
+};
+
+axios(config)
+.then(function (response) {
+  console.log(JSON.stringify(response.data));
+})
+.catch(function (error) {
+  console.log(error);
+});
+
+
