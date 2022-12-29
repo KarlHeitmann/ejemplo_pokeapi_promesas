42ffbbbc460f5703bb4a020924107aaef635a8a2
Karl Heitmann <khf1988@gmail.com>
2022-12-28 21:21:17 -0300

    mapeamos el arreglo completo de movimientos en uno más pequeñito

--- index.js
+++ index.js
@@ -6,20 +6,42 @@ const axios = require('axios');
 // import axios from "axios"
 console.log("Hola mundo")
 
+// OPERACIONES FUNDAMENTALES DE LOS ARREGLOS:
+//
+// 1. map: MAPEAR, toma un arreglo, y cada uno de sus elementos, lo CONVIERTE en otro elemento. El arreglo de salida tiene el mismo tamaño del arreglo de entrada
+// 2. filter: FILTRA, toma un arreglo, le aplica un FILTRO, ¿qué hace ese filtro? te devuelve OTRO arreglo cuyos elementos serán <= que el tamaño del arreglo original/entrada
+// 3. reduce: Toma un arreglo => lo lleva a un ESCALAR.
+
+const arreglo_1 = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+// con reduce, calcular la suma de todos los elementos del arreglo.
 
 
-var config = {
-  method: 'get',
-  url: 'https://pokeapi.co/api/v2/pokemon/ditto',
-  headers: { }
-};
+function main() {
+  var config = {
+    method: 'get',
+    url: 'https://pokeapi.co/api/v2/pokemon/pikachu',
+    headers: { }
+  };
 
-axios(config)
-.then(function (response) {
-  console.log(JSON.stringify(response.data));
-})
-.catch(function (error) {
-  console.log(error);
-});
+  axios(config)
+  .then(function (response) {
+    // console.log(response.data);
+    // console.log(response.data.moves);
+    // response.data.moves.map(move => console.log())
+    const nombres_movimientos = response.data.moves.map(move => {
+      return { name: move.move.name, url: move.move.url }
+    })
 
+    // const nombres_movimientos = response.data.moves.map(move => { name: move.move.name, url: move.move.url })
+    console.log(nombres_movimientos);
+    // console.log(JSON.stringify(response.data));
+
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+}
+
+main()
 
